fba19a09c35fa2c0f7acca538ce9bd96f6d2ff2d
damakvarshney <damakvarshney@gmail.com>
2020-11-05 00:18:11 +0530

    11/5/2020 Spade
    4 Components, 1 assets and Styles

--- src/components/atoms/AppBackground.js
+++ src/components/atoms/AppBackground.js
@@ -1,4 +1,5 @@
 import React from 'react';
+<<<<<<< Updated upstream
 import { ImageBackground, Dimensions } from 'react-native';
 
 const {width, height} = Dimensions.get('window')
@@ -6,6 +7,13 @@ const {width, height} = Dimensions.get('window')
 export default AppBackground = ({children, image}) => {
   return (
     <ImageBackground style={{width, height}} source={require('../../assets/images/appBackground.png')}>
+=======
+import {ImageBackground} from 'react-native';
+
+export default AppBackground = ({children, image}) => {
+  return (
+    <ImageBackground style={{flex: 1}} source={image}>
+>>>>>>> Stashed changes
       {children}
     </ImageBackground>
   );
--- src/styles/common.js
+++ src/styles/common.js
@@ -1,5 +1,33 @@
+<<<<<<< Updated upstream
 import {Platform} from 'react-native';
 import colors from '../config/colors';
 
 export default {
+=======
+export default {
+  colors: {
+    primary: '#1E90FF',
+    secondary: '#4ecdc4',
+    black: '#000',
+    white: '#fff',
+    medium: '#6e6969',
+    light: '#f8f4f4',
+    dark: '#0c0c0c',
+    danger: '#ff5252',
+    blurDodger: 'rgba(31, 143, 255, 0.69)',
+    blurRed: 'rgba(255, 31, 31, 0.69)',
+    blurGreen: 'rgba(43, 171, 152, 0.8)',
+    silverGrey: '#d6d6d6',
+    dodgerBlue: '#1E90FF',
+    purple: '#5A17A4',
+
+    brightPurple: '#8C1AFF',
+  },
+  text: {
+    color: '#0c0c0c',
+    fontSize: 16,
+    padding: 5,
+    fontFamily: Platform.OS === 'android' ? 'Roboto' : 'Avenir',
+  },
+>>>>>>> Stashed changes
 };
