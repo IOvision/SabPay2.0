25284c4ef84b7564d2a0e63182d6578127137c36
Rakshit Singh <rakshit.lko@gmail.com>
2020-11-06 17:00:19 +0530

    added SearchBar

--- App.js
+++ App.js
@@ -2,11 +2,19 @@ import React from 'react';
 import {
   View, Text, StyleSheet
 } from 'react-native';
+import SearchBar from './src/components/molecules/SearchBar'
 
 function App() {
+  const [test, setTest] = React.useState("")
+  console.log(test)
   return (
-    <View>
+    <View style={{flex: 1, backgroundColor: 'grey'}}>
       <Text>Hello World</Text>
+      <SearchBar 
+        width={300} 
+        value={test} 
+        onChangeText={setTest}
+      />
     </View>
   )
 }
---
+++ src/styles/SearchBarStyle.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native'
+
+const SearchBar = StyleSheet.create({
+    background: {
+        flexDirection: 'row',
+        backgroundColor: 'white',
+        borderRadius: 10,
+        alignItems: 'center',
+        paddingHorizontal: 5
+    }
+})
+
+export default SearchBar
