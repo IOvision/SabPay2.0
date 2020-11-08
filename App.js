df4cc84c95547d8a9200f09dabf017eb02b02a3d
Rakshit Singh <rakshit.lko@gmail.com>
2020-11-08 20:28:40 +0530

    added bottom sheet and redux

--- App.js
+++ App.js
@@ -1,19 +1,52 @@
-import React from 'react';
-import {
-  View, Text, StyleSheet
-} from 'react-native';
-import Root from './src/navigation/Root'
+import * as React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import Animated from 'react-native-reanimated';
+import BottomSheet from 'reanimated-bottom-sheet';
 
-function App() {
-  return (
-    <View style={{flex: 1, backgroundColor: 'grey'}}>
-      <Root />
+export default function App() {
+  const renderContent = () => (
+    <View
+      style={{
+        backgroundColor: 'purple',
+        padding: 16,
+        height: 450,
+      }}
+    >
+      <Text style={{
+        fontSize: 24, 
+        fontWeight: 'bold', 
+        alignSelf: 'center',
+        color: 'white'
+        }}
+      >Profile</Text>
     </View>
-  )
+  );
+
+  const sheetRef = React.useRef(null);
+
+  return (
+    <>
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: 'papayawhip',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <Button
+          title="Open Bottom Sheet"
+          //snap to works on indedx of array, here snapTo(0) = snapTo 450
+          onPress={() => sheetRef.current.snapTo(0)}
+        />
+      </View>
+      <BottomSheet
+        ref={sheetRef}
+        //height of bottom sheet, need to be in decreasing order
+        snapPoints={[450, 100, 100]}
+        borderRadius={20}
+        renderContent={renderContent}
+      />
+    </>
+  );
 }
-
-const styles = StyleSheet.create({
-  
-});
-
-export default App
